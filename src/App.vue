86c57eebb6b64aababb8c2cc92d68e59cd76014b
Vikranth Kumar <vshiv2@unh.newhaven.edu>
2023-12-03 20:21:28 -0500

    minor change

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,34 @@
 </template>
 
 <script>
-// Step 1 : you import the component
-import BillionairesHeader from './components/BillionairesHeader.vue'
-import BillionairesList from './components/BillionairesList.vue'
+    // Step 1 : you import the component
+    import BillionairesHeader from './components/BillionairesHeader.vue'
+    import BillionairesList from './components/BillionairesList.vue'
 
-export default{
-  name: 'App',
-  // Step 2: register the component
-  components:{
-    BillionairesHeader,
-    BillionairesList 
-  },
-  data(){
-    return {
-      billionaires: []
+    export default{
+      name: 'App',
+      // Step 2: register the component
+      components:{
+        BillionairesHeader,
+        BillionairesList 
+      },
+      data(){
+        return {
+          billionaires: []
+        }
+      },
+      methods:{
+        async fetchBillionaires(){
+          const res = await fetch("https://final-node-oypw.onrender.com/api")
+          const data = await res.json()
+          console.log(data)
+          return data[0].billionaires
+        }
+      },
+      async created(){
+        this.billionaires = await this.fetchBillionaires()
+      }
     }
-  },
-  methods:{
-    async fetchBillionaires(){
-      const res = await fetch("https://final-node-oypw.onrender.com/api")
-      const data = await res.json()
-      console.log(data)
-      return data[0].billionaires
-    }
-  },
-  async created(){
-    this.billionaires = await this.fetchBillionaires()
-  }
-}
 </script>
 
 <style>
